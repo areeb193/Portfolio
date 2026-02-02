66ad47239608e7024e756a2f13ab66a86afa19e2
areeb193 <fa22-bse-193@cuilahore.edu.pk>
2026-02-02 22:37:26 +0500

    Refactor Navbar component to wrap social media icons in anchor tags for better accessibility and functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,18 @@ const Navbar = () => {
     <nav className=" mb-20 flex items-center justify-between py-6">
       
       <div className="m-8 flex items-center justify-center gap-4 text-2xl text-white">
-        <FaLinkedin />
-        <FaFacebook />
-        <FaGithub />
-        <FaInstagram />
+        <a href="https://www.linkedin.com/in/areeb-zakir/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://www.facebook.com/areeb.zakir.35" target="_blank" rel="noopener noreferrer">
+          <FaFacebook />
+        </a>
+        <a href="https://github.com/areeb193" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/areeb.zakir.96/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
